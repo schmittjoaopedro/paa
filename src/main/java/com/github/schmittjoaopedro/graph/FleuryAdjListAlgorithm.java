ff04f4c577efc95a463c3dc6ee77f0721252335d
schmittjoaopedro <schmittjoaopedro@gmail.com>
2018-05-05 18:40:34 -0300

    Fleury's Algorithm

--- src/main/java/com/github/schmittjoaopedro/graph/FleuryAdjListAlgorithm.java
+++ src/main/java/com/github/schmittjoaopedro/graph/FleuryAdjListAlgorithm.java
@@ -21,7 +21,7 @@ public class FleuryAdjListAlgorithm {
     };
 
     public static void main(String[] args) throws Exception {
-        for(String problem : problems) {
+        for (String problem : problems) {
             String graphLines[] = FileUtils.readFileToString(new File(problem), "UTF-8").split("\n");
             processGraph(graphLines, problem);
         }
@@ -30,17 +30,17 @@ public class FleuryAdjListAlgorithm {
     public static void processGraph(String[] graphLines, String problem) {
         int V = Integer.valueOf(graphLines[0].replaceAll("\\|V\\|=", ""));
         Graph graph = new Graph();
-        for(int l = 1; l < graphLines.length; l++) {
+        for (int l = 1; l < graphLines.length; l++) {
             int i = Integer.valueOf(graphLines[l].replaceAll("\\(", "").replaceAll(",.*?\\)", ""));
             int j = Integer.valueOf(graphLines[l].replaceAll("\\)", "").replaceAll("\\(.*?,", ""));
             graph.addEdge(i, j);
         }
         int oddCount = getOddVerticesCount(graph);
 
-        if(oddCount == 0) {
+        if (oddCount == 0) {
             System.out.println(problem + " has 0 odd vertices = Euler cycle.");
             printRoute(getEulerPath(graph, false));
-        } else if(oddCount == 2) {
+        } else if (oddCount == 2) {
             System.out.println(problem + " has 2 odd vertices = Euler path.");
             printRoute(getEulerPath(graph, true));
         } else {
@@ -50,7 +50,7 @@ public class FleuryAdjListAlgorithm {
 
     public static void printRoute(List<Node> path) {
         StringBuilder route = new StringBuilder();
-        for(Node vertex : path) {
+        for (Node vertex : path) {
             route.append(vertex.value).append("-");
         }
         route.deleteCharAt(route.length() - 1);
@@ -59,8 +59,8 @@ public class FleuryAdjListAlgorithm {
 
     public static int getOddVerticesCount(Graph graph) {
         int oddCount = 0; // O(1)
-        for(Node node : graph.getNodes()) { // O(|V|)
-            if(node.degree() % 2 == 1) { // O(1)
+        for (Node node : graph.getNodes()) { // O(|V|)
+            if (node.degree() % 2 == 1) { // O(1)
                 oddCount++; // O(1)
             }
         }
@@ -68,8 +68,8 @@ public class FleuryAdjListAlgorithm {
     }
 
     public static Node getEulerPathOrigin(Graph graph) {
-        for(Node node : graph.getNodes()) { // O(|V|)
-            if(node.degree() % 2 == 1) { // O(1)
+        for (Node node : graph.getNodes()) { // O(|V|)
+            if (node.degree() % 2 == 1) { // O(1)
                 return node; // O(1)
             }
         }
@@ -79,15 +79,15 @@ public class FleuryAdjListAlgorithm {
     /**
      * No melhor caso temos O(|E|^2).
      * No pior caso temos O(|V|) + O(|E|^2) = O(|E|^2).
-     *
+     * <p>
      * Assim a complexidade de tempo de pior caso é de O(|E|^2).
-     *
+     * <p>
      * A complexidade de espaço do pior caso pertence ao getEulerPath O(|E|^2),
      * porque a função que busca origem é de O(1).
      */
     public static List<Node> getEulerPath(Graph graph, boolean isPath) {
         List<Node> path = new ArrayList<Node>(); // O(1)
-        if(isPath) { // O(1)
+        if (isPath) { // O(1)
             path.add(getEulerPathOrigin(graph)); // O(|V|)
             getEulerPath(graph, path, path.get(0));  // O(|E|^2)
         } else {
@@ -100,28 +100,28 @@ public class FleuryAdjListAlgorithm {
     /**
      * A função getEulerPath recebe um nó origem e executa uma verificação de isBridge para cada
      * nó adjacente do nó origem.
-     *
+     * <p>
      * Como para cada nó existem |degree(V_{from})| arestas, e a contagem de cada aresta é feita duas
      * vezes, (i,j) e (j,i). No pior caso iremos percorrer todas as arestas duas vezes 2|E| = O(|E|)
      * invocando a função is bridge, que tem custo O(|E|), em cada iteração.
-     *
+     * <p>
      * No melhor caso, em que para cada vértice a primeira aresta da lista de adjacências sempre satisfaz
      * a condição de não ponte e o circuito é fechado ao atingir o último vértice, teremos |V| * O(|E|)
-     *
+     * <p>
      * No pior caso, em que todas as arestas da lista de adjacência dos vértices são percorridas,
      * temos complexidade de |E| * O(|E|) = O(|E|^2).
-     *
+     * <p>
      * Pela análise de pior caso a complexidade é de tempo é de O(|E|^2).
-     *
+     * <p>
      * A análise da complexidade de espaço dessa função, por ser um processo recursivo e invocar uma DFS,
      * no pior caso tem complexidade O(|V|) + O(|V|) = O(|V|). Esse caso, 2*O(|V|) pode ocorrer porque todos
      * os vértices menos um podem estar no caminho corrente porém para fechar o ciclo ainda são necessários
      * voltar por todos os vértices usando arestas diferentes.
      */
     public static void getEulerPath(Graph graph, List<Node> path, Node from) {
-        Node[] nodes = from.adjacency.values().toArray(new Node[] {}); // O(1)
+        Node[] nodes = from.adjacency.values().toArray(new Node[]{}); // O(1)
         for (Node to : nodes) { // O(degree(V_{from})
-            if(!isBridge(graph, from, to)) { // O(|E|)
+            if (!isBridge(graph, from, to)) { // O(|E|)
                 path.add(to); // O(1)
                 graph.delEdge(from, to); // O(1)
                 getEulerPath(graph, path, to); // O(1)
@@ -134,22 +134,22 @@ public class FleuryAdjListAlgorithm {
      * A função isBridge executa uma verificação para determinar se a visita da aresta
      * (from, to) não irá deixar o grafo disjunto. Essa verificação é necessária para
      * garantir as propriedades do caminho e ciclo euleriano.
-     *
+     * <p>
      * Inicialmente é executado a DFS para determinar a quantidade de nós que podem ser
      * alcançados a partir do nó from não considerando a remoção da aresta (from, to).
      * Então é removido a aresta (from, to) e executado o DFS para recalcular a quantidade
      * de nós que podem ser atingidos sem essa aresta.
      * Se a remoção da aresta reduz a cobertura dos vértices então essa aresta é ponte, o
      * que significa que ela irá separar o grafo e tornar uma das partes inacessível.
-     *
+     * <p>
      * Como essa rotina basicamente executa duas DFS, o custo no pior caso a complexidade
      * de tempo é de O(|E|) + O(|E|) = O(|E|).
-     *
+     * <p>
      * Como esse método não é recursivo porém invoca uma DFS que tem custo de O(|V|), a
      * complexidade de espaço desse método é de O(|V|).
      */
     public static boolean isBridge(Graph graph, Node from, Node to) {
-        if(from.adjacency.size() == 1) { // O(1)
+        if (from.adjacency.size() == 1) { // O(1)
             return false; // O(1)
         }
         int bridgeCount = getReachableNodesCount(new HashSet<Node>(), to); // O(|E|)
@@ -164,35 +164,35 @@ public class FleuryAdjListAlgorithm {
     /**
      * A busca em profundidade DFS (Depth-First Search) geralmente é aplicada para
      * encontrar todos os vértices de um grafo.
-     *
+     * <p>
      * Para cada vértice um processo recursivo é iniciado onde visita-se, de forma sequencial,
      * os filhos adjacentes do vértice corrente que ainda não foram visitados.
-     *
+     * <p>
      * Nesse caso, a execução do DFS é aplicado somente na estrutura do nó "from" para
      * encontrar todos os vértices que podem ser encontrados a partir dele. Como estamos
      * considerando um grafo simples, para cada vértice será executado |degree(V)|
      * comparações.
-     *
+     * <p>
      * sum_{v \in V} |degree(v)| = 2|E| = O(|E|)
-     *
+     * <p>
      * A somatória resulta em 2|E| porque para um dado vértice i e um vértice j, o grau de
      * i considera j na contagem e o grau de j considera i na sua contagem, dessa forma
      * essa aresta é encontrada duas vezes.
-     *
+     * <p>
      * Nota: esse custo de O(|E|) só é garantido pelo uso de um HashSet que garante o tempo de
      * consulta de inserção de nós visitado com complexidade de O(1).
-     *
+     * <p>
      * Dessa forma a complexidade de tempo é de O(|E|)
-     *
+     * <p>
      * Como o processo recursivo é aberto para cada vértice, temos que a complexidade de espaço
      * é O(|V|).
      */
     public static int getReachableNodesCount(Set<Node> visited, Node from) {
         int count = 1; // O(1)
         visited.add(from); // O(1)
-        Node[] nodes = from.adjacency.values().toArray(new Node[] {}); // O(1)
+        Node[] nodes = from.adjacency.values().toArray(new Node[]{}); // O(1)
         for (Node to : nodes) { // O(degree(from))
-            if(!visited.contains(to)) { // O(1)
+            if (!visited.contains(to)) { // O(1)
                 count = count + getReachableNodesCount(visited, to); // O(1)
             }
         }
